cb3115852db82702accf8ed58117b2ced7601971
kimgorok <kjyse2@naver.com>
2023-06-26 19:56:23 +0900

    FileHover

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,13 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <details className=" open:text-white open:bg-indigo-400">
-        <summary className="select-none cursor-pointer">
-          What is my fav. food.
-        </summary>
-        <span className="selection:bg-indigo-500 selection:text-white">
-          김치
-        </span>
-      </details>
+      <input
+        type="file"
+        className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border-2  file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
+      />
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400 first-line:text-red-300">
+        Hello everyone
+      </p>
     </div>
   );
 };
