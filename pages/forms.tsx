ecb2731c96f4299b8116cb25c89d3c6bac729318
kimgorok <kjyse2@naver.com>
2023-07-05 03:47:27 +0900

    ReactHookForm

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,57 +1,76 @@
 import { useState } from "react";
+import { FieldErrors, useForm } from "react-hook-form";
+
+// React Hook Form의 목표
+// Less Code
+// Better Validation
+// Better Errors (set, clear, display)
+// Have Control Over Inputs
+// Don't Deal With Event
+// Easier Inputs
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+  errors?: string;
+}
 
 export default function Forms() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formErrors, setFormErrors] = useState("");
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch, // watch("name") 등으로 입력한 값을 얻어냄
+    setError,
+    setValue,
+    reset,
+    resetField, //특정필드만 초기화
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+  const onValid = (data: LoginForm) => {
+    console.log("난 유효해");
+    setError("username", { message: "존재하는 유저네임입니다" }); // 오류를 수동으로 설정
+    reset();
   };
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmail(value);
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-  };
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (username === "" || email === "" || password === "") {
-      setFormErrors("모두 필수임");
-    }
-  };
+  console.log(errors);
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        value={username}
-        onChange={onUsernameChange}
+        {...register("username", {
+          required: "이름이 필요해",
+          minLength: {
+            message: "5글자 이상",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
-        minLength={5}
       />
+      {errors.errors?.message}
       <input
-        value={email}
-        onChange={onEmailChange}
+        {...register("email", {
+          required: "이메일 필요",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "쥐메일 안돼용",
+          },
+        })}
         type="email"
         placeholder="Email"
-        required
+        className={`${Boolean(errors.email?.message) ? "border-red-500" : ""}`}
       />
+      {errors.email?.message}
       <input
-        value={password}
-        onChange={onPasswordChange}
+        {...register("password", {
+          required: "비밀번호 필요",
+        })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
