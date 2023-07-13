2af6fc25efff6f9a384af28b534a26af372dd5ca
kimgorok <kjyse2@naver.com>
2023-07-14 04:34:29 +0900

    ~#11.2 Upload API

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+// 클라이언트가 없으면 새로 만들고 있으면 기존꺼를 global.client에 저장
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
---
+++ pages/api/products/index.ts
@@ -0,0 +1,40 @@
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "xx",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  // 생성한 product를 return 했으니 그 product의 id를 받아서
+  // 유저를 그 페이지로 redirect 시킴
+  res.json({
+    ok: true,
+    product,
+  });
+}
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  })
+);
